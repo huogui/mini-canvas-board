9d3458558970e0d6360dcd7cc94e01b4572604e1
Rail <wanglifei@yiqicai.com>
2022-05-24 10:56:07 +0800

    feat: project fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,10 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import pkg from './package.json';
-import { getName } from 'pkg-get';
 
-const name = getName(pkg.name);
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: `/${pkg.name}/`,
   plugins: [ vue() ],
-  build: {
-    outDir: './lib',
-    lib: {
-      entry: 'src/index.ts',
-      name,
-      formats: [ 'umd', 'es' ],
-    },
-    rollupOptions: {
-      external: [ 'vue' ],
-      output: {
-        globals: {
-          vue: 'Vue',
-        },
-        exports: 'named',
-      },
-    },
-  },
 });
